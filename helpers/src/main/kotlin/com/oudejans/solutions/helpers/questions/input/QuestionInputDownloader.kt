5ac09972764db604d533773948be01023d036955
Jaap Oudejans <jaap@oudejans.solutions>
2022-12-01 09:06:47 +0100

    Added documentation explaining why we read the file locally

--- helpers/src/main/kotlin/com/oudejans/solutions/helpers/questions/input/QuestionInputDownloader.kt
+++ helpers/src/main/kotlin/com/oudejans/solutions/helpers/questions/input/QuestionInputDownloader.kt
@@ -2,6 +2,7 @@ package com.oudejans.solutions.helpers.questions.input
 
 class QuestionInputDownloader {
 
+    // Note: we cannot download the input directly from the website, they prefer we download the file once, and then go from there. Hence the local file reading
     fun getInput(day: Int): String =
         javaClass.getResource("/input/day-$day.txt")?.readText()
             ?: error("Couldn't read input for day $day, maybe the input file is not downloaded yet to resources/input/day-$day.txt")
