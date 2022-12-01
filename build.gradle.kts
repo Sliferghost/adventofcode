f3c5cf53890c433a6f08bced5c87123aa464aae1
Jaap Oudejans <jaap@oudejans.solutions>
2022-12-01 08:35:13 +0100

    Setup testing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,41 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.7.22"
+}
+
 group = "com.oudejans.solutions"
 version = "1.0.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+}
+
+subprojects {
+    apply(plugin = "kotlin")
+
+    repositories {
+        mavenCentral()
+    }
+
+    dependencies {
+        implementation(kotlin("stdlib-jdk8"))
+
+        testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.1")
+        testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.1")
+        testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.25")
+    }
+
+    val test: Test by tasks
+    test.useJUnitPlatform()
+
+    val compileKotlin: KotlinCompile by tasks
+    compileKotlin.kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    val compileTestKotlin: KotlinCompile by tasks
+    compileTestKotlin.kotlinOptions {
+        jvmTarget = "1.8"
+    }
 }
--- day-01/build.gradle.kts
+++ day-01/build.gradle.kts
@@ -1,27 +1,2 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
-plugins {
-    kotlin("jvm") version "1.7.22"
-}
-
 dependencies {
-    implementation(kotlin("stdlib-jdk8"))
-
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.1")
-    testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.25")
-}
-
-repositories {
-    mavenCentral()
-}
-
-val compileKotlin: KotlinCompile by tasks
-compileKotlin.kotlinOptions {
-    jvmTarget = "1.8"
-}
-
-val compileTestKotlin: KotlinCompile by tasks
-compileTestKotlin.kotlinOptions {
-    jvmTarget = "1.8"
 }
